0e54af2380664b743b4b49cae97380ff51f0943e
Konrad-Ł <lysek.konrad@gmail.com>
2022-11-09 22:18:00 +0100

    Added ship rotation, constantly looking at mouse position

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -7,9 +7,12 @@ public class InputHandler : MonoBehaviour
 {
     [SerializeField]
     private float flySpeed = 10f;
+    [SerializeField]
+    private GameObject shipSprite;
     void Update()
     {
         Move();
+        Rotate();
     }
 
     private void Move()
@@ -17,5 +20,11 @@ public class InputHandler : MonoBehaviour
         float horizontalMove = Input.GetAxis("Horizontal");
         float verticalMove = Input.GetAxis("Vertical");
         transform.Translate(horizontalMove * flySpeed * Time.deltaTime, verticalMove * flySpeed * Time.deltaTime, 0);
+    }
+
+    private void Rotate()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        shipSprite.transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
     }
 }
